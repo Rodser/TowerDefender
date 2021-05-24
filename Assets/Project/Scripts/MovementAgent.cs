3eb7a3372538c543aabd32217a264065f0016d41
Rodser <63009193+Rodser@users.noreply.github.com>
2021-05-24 08:50:29 +0400

    Movement Agent

--- Assets/Project/Scripts/MovementAgent.cs
+++ Assets/Project/Scripts/MovementAgent.cs
@@ -1,18 +1,23 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class MovementAgent : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private const float TOLERANCE = 0.1f;
+    [SerializeField]
+    private float speed;
+    [SerializeField]
+    private Vector3 target;
 
-    // Update is called once per frame
-    void Update()
+
+    private void Update()
     {
+        float distancec = (target - transform.position).magnitude;
         
+        if (distancec < TOLERANCE)
+            return;
+
+        var dir = (target - transform.position).normalized;
+        var delta = dir * (speed * Time.deltaTime);
+        transform.Translate(delta);
     }
 }
